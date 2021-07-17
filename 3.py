224dd6c1ec9010e4d53e8fd6029954b2ab20db47
Peter Groß <pgross@newline.studio>
2021-07-18 00:49:55 +0200

    day 3b

--- 3.py
+++ 3.py
@@ -2,6 +2,20 @@ def is_valid_triangle(a, b, c):
     return a + b > c and a + c > b and b + c > a
 
 
+def part_b_reformat(triangle_list: list) -> list:
+    new_list = []
+    index = 0
+    while index + 2 < len(triangle_list):
+        new_list.extend([
+            (triangle_list[index][0], triangle_list[index + 1][0], triangle_list[index + 2][0]),
+            (triangle_list[index][1], triangle_list[index + 1][1], triangle_list[index + 2][1]),
+            (triangle_list[index][2], triangle_list[index + 1][2], triangle_list[index + 2][2]),
+        ])
+        index += 3
+
+    return new_list
+
+
 triangles = []
 with open('input/3') as f:
     for line in f:
@@ -9,6 +23,9 @@ with open('input/3') as f:
         temp = filter(lambda x: x != "", temp)
         temp = tuple(map(lambda x: int(x), temp))
         triangles.append(temp)
+
+# part b
+triangles = part_b_reformat(triangles)
 
 triangles = list(filter(lambda tri: is_valid_triangle(*tri), triangles))
 print(len(triangles))
