a7da91c1a0df34ed1f969ebf7171608fa95aeb87
Peter Groß <pgross@newline.studio>
2021-08-22 13:59:09 +0200

    day 7b

--- 7.py
+++ 7.py
@@ -1,21 +1,45 @@
 import re
 
 
-def has_abba(sequence: str) -> bool:
+# part a
+def get_abba(sequence: str) -> bool:
     seq_len = len(sequence)
     i = 0
     while i + 3 < seq_len:
         if sequence[i] == sequence[i + 3] and sequence[i + 1] == sequence[i + 2] and not sequence[i] == sequence[i + 1]:
             return True
         i += 1
-
     return False
 
 
 def sequences_have_abba(sequences: list) -> bool:
     for seq in sequences:
-        if has_abba(seq):
+        if get_abba(seq):
             return True
+    return False
+
+
+# part b
+def get_aba(sequence: str, aba: list = []) -> list:
+    seq_len = len(sequence)
+    i = 0
+    while i + 2 < seq_len:
+        if sequence[i] == sequence[i + 2] and not sequence[i] == sequence[i + 1]:
+            if not aba or sequence[i] == aba[1] and sequence[i + 1] == aba[0]:
+                # first char, second char, index to split at
+                return [sequence[i], sequence[i + 1], i + 1]
+        i += 1
+    return []
+
+
+def sequences_support_ssl(supernet: list, hypernet: list) -> bool:
+    for sup_seq in supernet:
+        while match := get_aba(sup_seq):
+            print(sup_seq)
+            for hyp_seq in hypernet:
+                if get_aba(hyp_seq, match):
+                    return True
+            sup_seq = sup_seq[match[2]:]
     return False
 
 
@@ -28,7 +52,7 @@ with open('input/7') as f:
         line = re.sub(pattern, '|', line)
         supernet_sequences = line.split('|')
 
-        if not sequences_have_abba(hypernet_sequences) and sequences_have_abba(supernet_sequences):
+        if sequences_support_ssl(supernet_sequences, hypernet_sequences):
             amount += 1
 
 print(amount)
